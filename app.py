1226a3e9b6320f6899d5bed6b2d18be05a7c74e8
virajnandalikar-sudo <virajnandalikar@gmail.com>
2026-03-23 10:53:56 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>🚀 Hello, this is your UI!</h1><p>Welcome to the app running in your browser.</p>"
+
+if __name__ == "__main__":
+    # Run on localhost:5000
+    app.run(host="0.0.0.0", port=5000, debug=True)
